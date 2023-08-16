ce7c70241ee57c0b6487d7181ec09a7477aa992d
Ruchika <bagalruchika@gmail.com>
2023-08-16 15:05:30 +0530

    one file has been updated!

--- Index.c
+++ Index.c
@@ -3,8 +3,22 @@ int Index(int array[], int size, int Value);
 
 
 int main() {
-    int array[] = {1, 2, 5, 9, 11};
-    int size = sizeof(array) / sizeof(array[0]); //
+    // int array[50];
+    int array[50];
+    int size;
+    printf("enter the size of array ");
+    scanf("%d", &size);
+    for (int i=0; i<size; i++){
+        printf("element %d is: ", i+1);
+        scanf("%d", &array[i]);
+    }
+    printf("your entered array is:");
+    for (int i=0; i<size; i++){
+        printf("%d", array[i]);
+    }
+    
+    size = sizeof(array) / sizeof(array[0]); 
+
 
     int Value;
     printf("Enter the value to search: ");
